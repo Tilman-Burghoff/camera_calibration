35b9e9bce66fe3d56e8d136c282c015bf70d4ea8
Tilman-Burghoff <tilmanburghoff@posteo.de>
2025-09-12 15:46:47 +0200

    switches R, t computation to not use inverse

--- linearized_solution.py
+++ linearized_solution.py
@@ -1,7 +1,8 @@
 # we assume data in the form (n_i, c_i, x_i), where n_i is the normal of the table and c_i its center expressed in the gripper frame.
-# x_i is a point of the pointcloud in the the camera frame in homogeneous coordinates. We try to find the transformation T from camera to gripper
-# such that all points lie on the table, ie n_i^T * (T * x_i - c_i) = 0 for all i. This can be rewritten as a linear system of equations by
-#  n_i^T * (T * x_i - c_i) = 0  <=> n_i^T * c_i = n_i^T * T * x_i = kron(n_i, x_i) * vec(T), which allows us to solve for vec(T) with least squares.
+# x_i is a point of the pointcloud in the the camera frame in homogeneous coordinates. We try to find the projection P from camera to gripper
+# such that all points lie on the table, ie n_i^T * (P * x_i - c_i) = 0 for all i. This can be rewritten as a linear system of equations by
+#  n_i^T * (P * x_i - c_i) = 0  <=> n_i^T * c_i = n_i^T * T * x_i = kron(n_i, x_i) * vec(P), which allows us to solve for vec(P) with least squares.
+# P then has the form P = [R^T, -R^T * t], from which we first recover R and t and then our transformation Q = [t, q] where q is the quaternion of R.
 
 import json
 import numpy as np
@@ -45,14 +46,16 @@ if __name__ == '__main__':
     n, c, x = load_data('camera_calibration_data.json')
     A = np.einsum('ij,ik->ijk', n, x).reshape(-1, 12) # each row is kron(n_i, x_i)
     b = np.einsum('ij,ij->i', n, c) # each row is n_i^T * c_i
-    T,_,_,_ = np.linalg.lstsq(A, b)
-    T = np.vstack([T.reshape(3, 4), [0, 0, 0, 1]])
-    T_inv = np.linalg.inv(T)
-    t = T_inv[:3, 3]
-    R = T_inv[:3, :3]
+    P,_,_,_ = np.linalg.lstsq(A, b)
+    P = P.reshape(3,4)
+    R = P[:3, :3].T
+    t = -R @ P[:3, 3]
     q = Rotation.from_matrix(R).as_quat()
     Q = np.round(t, 8).tolist() + np.round(q, 8).tolist()
-    print(Q) # returns [0.02007601, 0.05057013, 0.03766085, 0.00368336, 0.99988913, -0.00662298, 0.01281754]
+    print(Q) # returns [0.02063466, 0.05156388, 0.03764117, 0.00366712, 0.99988991, -0.00660188, 0.0127722]
+    # TODO: weirdly mostly correct, but the y translation is in the wrong direction and the rotation should 
+    # align y instead of x axis with the gripper. This might be due to the image coordinate frame?? 
+    # but since we use the pointcloud this shouldnt matter...
 
     C.setJointState(qHome)
     cam = C.addFrame('computed_cam', 'l_gripper').setShape(ry.ST.marker, [.1])
