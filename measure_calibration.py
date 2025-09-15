070dc9779f91e499d8b736b9353ba2ebfbead7c5
tilman <tilmanburghoff@posteo.de>
2025-09-15 18:17:55 +0200

    adds global transformation

--- measure_calibration.py
+++ measure_calibration.py
@@ -27,21 +27,21 @@ def main():
     C.addFile(ry.raiPath("scenarios/pandaSingle_camera.g"))
 
     bot = ry.BotOp(C, True)
-    target = C.addFrame("target").setPosition([-.5, .2, .65])
+    C.addFrame("target").setPosition([-.5, .2, .65])
 
     path = look_at_target(C, "target", height=.3)
 
     bot.moveTo(path[-1])
 
-    rgb, _, points = bot.getImageDepthPcl("l_cameraWrist", globalCoordinates=True)
+    rgb, _, points = bot.getImageDepthPcl("l_cameraWrist")
 
     v_coords = np.linspace(0,1, points.shape[0])
     u_coords = np.linspace(0,1, points.shape[1])
     u, v = np.meshgrid(u_coords, v_coords)
     uv = np.stack([u, v, np.ones_like(u)], axis=-1)
     X = uv.reshape(-1, 3)
-    Y = points.reshape(-1, 3)
-
+    cam_transform = C.getFrame('l_cameraWrist').getTransform()
+    Y = points.reshape(-1, 3) @ cam_transform[:3, :3].T + cam_transform[:3, 3]
     P = Y.T @ X @ np.linalg.inv(X.T @ X)
 
     pcl = C.addFrame("pcl", "l_cameraWrist")
