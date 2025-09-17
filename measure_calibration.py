d63b64b5c3bba7ea8b28b2941acd76b671a791a3
Tilman-Burghoff <tilmanburghoff@posteo.de>
2025-09-17 16:42:34 +0200

    improves metrics (WIP)
    - changes projection calculation to seperate intrinsics and extrinsics (result still difficult to reason about)
    - adds span parameter measuring distance between highest and lowest point

--- measure_calibration.py
+++ measure_calibration.py
@@ -1,6 +1,7 @@
 import robotic as ry
 import numpy as np
 
+camera = 'l_cameraWrist_o' # new calib, use 'l_cameraWrist_o' for old calib
 
 def look_at_target(C, target_name, height):
     komo = ry.KOMO(C, 1, 1, 0, True)
@@ -33,26 +34,30 @@ def main():
 
     bot.moveTo(path[-1])
 
-    rgb, _, points = bot.getImageDepthPcl("l_cameraWrist")
+    rgb, depth, points = bot.getImageDepthPcl("l_cameraWrist")
 
     v_coords = np.linspace(0,1, points.shape[0])
     u_coords = np.linspace(0,1, points.shape[1])
     u, v = np.meshgrid(u_coords, v_coords)
-    uv = np.stack([u, v, np.ones_like(u)], axis=-1)
-    X = uv.reshape(-1, 3)
-    cam_transform = C.getFrame('l_cameraWrist').getTransform()
+    cam_pixels = np.stack([u, v, depth, np.ones_like(u)], axis=-1)
+    X = cam_pixels.reshape(-1, 4)
+    cam_transform = C.getFrame(camera).getTransform()
     Y = points.reshape(-1, 3) @ cam_transform[:3, :3].T + cam_transform[:3, 3]
     P = Y.T @ X @ np.linalg.inv(X.T @ X)
+    t = P[:,3]
+    R, K = np.linalg.qr(P[:,:4])
 
-    pcl = C.addFrame("pcl", "l_cameraWrist")
-    pcl.setPointCloud(points, rgb)
+    pcl = C.addFrame("pcl")
+    pcl.setPointCloud(Y, rgb.reshape(-1,3))
     C.view(True)
     del bot
     del C
 
     print("z std:", np.std(Y[:,2]))
-    print("Calibration matrix:")
-    print(np.round(P,3))
+    lq, uq = np.percentile(Y[:,2], [.01,.99])
+    print('Span:' , np.round(uq-lq,4), 'm')
+    print("Projection Matrix:")
+    print(np.round(np.hstack([R, t[:,None]]),3))
 
 
 if __name__=="__main__":
