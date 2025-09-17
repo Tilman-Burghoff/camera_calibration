31b9d851ec0565ba708fb0057537d7da0e7aee12
Tilman-Burghoff <tilmanburghoff@posteo.de>
2025-09-17 17:48:49 +0200

    adds multiple rounds for averaging

--- get_marker_gt.py
+++ get_marker_gt.py
@@ -4,6 +4,7 @@ from robotic.src import h5_helper
 import json
 
 MARKER_IDS = [4, 6, 14] # ids of the markers to record
+ROUNDS = 3
 
 ry.params_add({
     'bot/useOptitrack': True,
@@ -27,26 +28,29 @@ C.addFrame('origin').setPosition(origin).setShape(ry.ST.marker, [.05])
 C.addFrame('table', 'origin').setRelativePosition([0,0,-.05]).setShape(ry.ST.box, [2.3, 1.24, .11]).setColor([100,100,100,100])
 template_marker = C.addFrame('template_marker', 'origin').setShape(ry.ST.marker, [.5])
 
-results = {}
+results = []
 
 def opti_to_world(z):
     z = z - origin
     z[2] += .6
     return np.array([-z[1], z[0], z[2]])
 
-for id in MARKER_IDS:
-    bot.sync(C)
-    while bot.getKeyPressed() != ord('q'):
-        z = opti_to_world(template.getPosition()) + np.array([0,0,-.6])
-        template_marker.setRelativePosition(z)
-        bot.sync(C, viewMsg=f'Move template to marker {id}')
-    coords = opti_to_world(C.getFrame('template').getPosition())
-    results[id] = coords
-    print(f'marker {id} position: {np.round(coords, 3)}')
+for round in range(ROUNDS):
+    results.append({})
+    for id in MARKER_IDS:
+        bot.sync(C)
+        while bot.getKeyPressed() != ord('q'):
+            z = opti_to_world(template.getPosition()) + np.array([0,0,-.6])
+            template_marker.setRelativePosition(z)
+            bot.sync(C, viewMsg=f'Move template to marker {id}')
+        coords = opti_to_world(C.getFrame('template').getPosition())
+        results[round][id] = coords
+        print(f'marker {id} position: {np.round(coords, 3)}')
 
 
 h5 = h5_helper.H5Writer('marker_gt.h5')
-for id, pos in results.items():
+for id in MARKER_IDS:
+    pos = np.mean([results[r][id] for r in range(ROUNDS)], axis=0)
     h5.write(f'marker_{id}/position', pos, dtype='float32')
 
 manifest = {
